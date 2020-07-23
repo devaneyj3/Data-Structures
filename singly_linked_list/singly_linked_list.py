df69224f6598b1523644c0ab59534a5c9ed033ad
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-07-23 12:10:21 -0400

    passed singly linked lists

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, value, next = None):
         self.value = value
-        self.next = next #if next is not None else None
+        self.next = next if next is not None else None
     def get_value(self):
         return self.value
     
@@ -38,13 +38,15 @@ class LinkedList:
         self.tail = current
         return val
         
-    def remove_head(self):
-        if self.tail is None and self.head is None:
+    def remove_head(self): 
+        if not self.head:
             return 
         if not self.head.get_next():
             head = self.head
             self.head = None
+            self.tail = None
             return head.get_value()
+        
         val = self.head.get_value()
         self.head = self.head.get_next()
         return val
@@ -62,18 +64,33 @@ class LinkedList:
             current = current.get_next()
         return False
         
+
     def get_max(self):
-        # if we do not have a head
-        # 
-        if not self.head: 
+        # We're going to grab the initial value, from the head of the *LinkedList*
+        # This distinction is important because the LinkedList *is not aware of any of it's nodes*,
+        # except for the head and tail.
+        current = self.head 
+        # We're going to grab the initial value, from the head of the *LinkedList*
+        # This distinction is important because the LinkedList *is not aware of any of it's nodes*,
+        # except for the head and tail.
+        # from the `current` variable, meaning max value is equivalent to self.head.value.
+        max_val = current
+        # Next we check if the head is `None`, because if it is, the max value is None, unless
+        # there's only one node, in which case the max is the singular node's value
+        if self.head is None:
             return None
-        
-        if self.head is not None:
-            current = self.head
-            next_head = self.head.get_next()
-            
-        if current.value > next_head:
-            return current
-        
-newThing = LinkedList()
-print(newThing.get_max())
+        else:
+            # Next we check if the head is `None`, because if it is, the max value is None, unless
+            # there's only one node, in which case the max is the singular node's value
+            while current.next is not None: 
+                # We immediately set current to current.next, because we have the value from current in `max_val`,
+                # so, we want to go to our second node, to get a value to compare to the first.
+                current = current.next 
+                if current.value > max_val.value: 
+                    # Now that we have two values, we can compare them, and if the current.value (which is equivalent to current.next.value),
+                    # is greater than `max_val`,
+                    max_val = current.value # then we set max_val to that value.
+                    return max_val
+            # if head doesn't have a node next to it,'
+            max_val = self.head.value
+            return max_val
